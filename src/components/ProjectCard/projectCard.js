84f87e596354234e5efa2b8eab2a40d46b6d7825
horoiu <marian.horoiu@gmail.comgit>
2020-02-21 22:02:40 +0200

    Destructurate this.props to make JSX code cleaner

--- src/components/ProjectCard/projectCard.js
+++ src/components/ProjectCard/projectCard.js
@@ -5,22 +5,32 @@ import "./projectCard.css";
 class ProjectCard extends Component {
     render() {
         // console.log("projectCard this.props", this.props);
+
+        // We can destructurate the props here to make the JSX cleaner
+        // e.g.  const { titlu, tehnologie, descriere, id, deleteProject } = this.props;
+        // and the use them without the this.props '<h5>{titlu}</h5>'
+        const {
+            title,
+            technology,
+            description,
+            id,
+            deleteProject
+        } = this.props;
+
         return (
-            <>
-                <div className="projectCard d-inline-block btn">
-                    <h5>{this.props.title}</h5>
-                    <p>{this.props.tehnology}</p>
-                    <p>{this.props.description}</p>
-                    <MDBBtn
-                        color="indigo"
-                        className="float-right"
-                        id={this.props.id}
-                        onClick={this.props.deleteProject}
-                    >
-                        Delete
-                    </MDBBtn>
-                </div>
-            </>
+            <div className="projectCard d-inline-block btn">
+                <h5>{title}</h5>
+                <p>{technology}</p>
+                <p>{description}</p>
+                <MDBBtn
+                    color="indigo"
+                    className="float-right"
+                    id={id}
+                    onClick={deleteProject}
+                >
+                    Delete
+                </MDBBtn>
+            </div>
         );
     }
 }
